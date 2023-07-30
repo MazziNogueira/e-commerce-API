9312b8e03a70b89fbc68e0d772471884131d6f60
Marina Azzi Nogueira <mack@Macks-MacBook-Pro.local>
2023-07-30 18:52:48 -0300

    refact done for 4 endpoints

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,18 @@ app.get('/ping', (req: Request, res: Response) => {
 
 // GET All Users
 app.get('/users', (req: Request, res: Response) => {
-    res.status(200).send(usersList)
+    try {
+        res.status(200).send(usersList)
+    } catch (error) {
+        console.log(error)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        res.status(400).send('Erro inesperado')
+    }
+
 })
 
 // GET All Products
@@ -33,52 +44,137 @@ app.get('/users', (req: Request, res: Response) => {
 
 // GET All Products By Name
 app.get('/products', (req: Request, res: Response) => {
-    const productName = req.query.name as string
+    try {
+        const productName = req.query.name as string
 
-    if (productName) {
-        const result: Product[] = productsList.filter(
-            (product) => product.name.toLowerCase().includes(productName.toLowerCase())
-        )
+        if (productName) {
+            if (productName.length < 2) {
+                res.statusCode = 400
+                throw new Error('O termo buscado deve ter pelo menos dois caracteres.')
+            }
 
-        res.status(200).send(result)
-    } else {
-        res.status(200).send(productsList)
+            const result = productsList.filter((product) => {
+                return product.name.toLowerCase().includes(productName.toLowerCase())
+            })
+
+            res.status(200).send(result)
+        } else {
+            res.status(200).send(productsList)
+        }
+    } catch (error) {
+        console.log(error)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.status(200).send(productsList)
+        }
     }
 })
 
 // CREATE User
 app.post('/users', (req: Request, res: Response) => {
-    const { name, email, password } = req.body
+    try {
+        const { name, email, password } = req.body
 
-    const newUser: User = {
-        id: new Date().toISOString(),
-        name: name,
-        email: email,
-        password: password,
-        createdAt: new Date().toISOString()
+        if (!name || !email || !password) {
+            res.statusCode = 406
+            throw new Error('É obrigatório enviar "name", "email" e "password" no corpo da requisição.')
+        }
+
+        if (typeof name !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+            res.statusCode = 406
+            throw new Error('Os dados "name", "email" e "password" devem estar no formato "string".')
+        }
+
+        const checkEmail = usersList.filter((user) => {
+            return user.email === email
+        })
+
+        // filter retorna um array vazio caso não encontre nenhum user com email igual
+        if (checkEmail.length > 0) {
+            res.statusCode = 409
+            throw new Error('O email enviado já foi cadastrado.')
+        }
+
+        const newUser: User = {
+            id: new Date().toISOString(),
+            name: name,
+            email: email,
+            password: password,
+            createdAt: new Date().toISOString()
+        }
+
+        usersList.push(newUser)
+
+        res.status(201).send('Cadastro realizado com sucesso')
+    } catch (error) {
+        console.log(error)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send('Erro inesperado.')
+        }
     }
-
-    usersList.push(newUser)
-
-    res.status(201).send('Cadastro realizado com sucesso')
 })
 
 
 // CREATE Product
 app.post('/products', (req: Request, res: Response) => {
-    const { name, price, description, imageUrl } = req.body
+    try {
+        const { name, price, description, imageUrl } = req.body
 
-    const newProduct: Product = {
-        id: new Date().toISOString(),
-        name: name,
-        price: price,
-        description: description,
-        imageUrl: imageUrl
+        if (!name || !price || !description || !imageUrl) {
+            res.statusCode = 406
+            throw new Error('É obrigatório enviar "name", "price", "description" e "imageUrl" no corpo da requisição.')
+        }
+
+        if (typeof name !== 'string' || typeof description !== 'string' || typeof imageUrl !== 'string') {
+            res.statusCode = 406
+            throw new Error('Os dados "name", "description" e "imageUrl" devem estar no formato "string".')
+        }
+
+        if (typeof price !== 'number') {
+            res.statusCode = 406
+            throw new Error('O dado "price" deve estar no formato "number"')
+        }
+
+        const newProduct: Product = {
+            id: new Date().toISOString(),
+            name: name,
+            price: price,
+            description: description,
+            imageUrl: imageUrl
+        }
+
+        productsList.push(newProduct)
+
+        res.status(201).send('Produto cadastrado com sucesso')
+
+    } catch (error) {
+        console.log(error)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send('Erro inesperado.')
+        }
+
     }
 
-    productsList.push(newProduct)
-
-    res.status(201).send('Produto cadastrado com sucesso')
 })
 
 // DELETE User By Id
